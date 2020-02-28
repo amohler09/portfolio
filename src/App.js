6b8533717760b9ce94846ad29d7e10b046d9bf29
Amber Mohler <ambermohler@iMac.local>
2020-02-28 10:42:32 -0500

    created landing page

--- src/App.js
+++ src/App.js
@@ -1,56 +1,27 @@
 import React from 'react';
-import { useEffect, useState } from 'react';
-import './App.css';
+import { Route, Switch } from 'react-router-dom'
+
+import MainPage from './components/MainPage'
+import Projects from './components/Projects'
+import Resume from './components/Resume'
+
+import styled  from 'styled-components'
+
+const MainDiv = styled.div`
+  margin: auto;
+`;
 
 function App() {
-  const [date, setDate] = useState(null);
-  useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.text();
-      setDate(newDate);
-    }
-    getDate();
-  }, []);
+  
   return (
-    <main>
-      <h1>Create React App + Go API</h1>
-      <h2>
-        Deployed with{' '}
-        <a
-          href="https://zeit.co/docs"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          ZEIT Now
-        </a>
-        !
-      </h2>
-      <p>
-        <a
-          href="https://github.com/zeit/now/tree/master/examples/create-react-app"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          This project
-        </a>{' '}
-        was bootstrapped with{' '}
-        <a href="https://facebook.github.io/create-react-app/">
-          Create React App
-        </a>{' '}
-        and contains three directories, <code>/public</code> for static assets,{' '}
-        <code>/src</code> for components and content, and <code>/api</code>{' '}
-        which contains a serverless <a href="https://golang.org/">Go</a>{' '}
-        function. See{' '}
-        <a href="/api/date">
-          <code>api/date</code> for the Date API with Go
-        </a>
-        .
-      </p>
-      <br />
-      <h2>The date according to Go is:</h2>
-      <p>{date ? date : 'Loading date...'}</p>
-    </main>
+   <MainDiv>
+    <Switch>
+    <Route path='/projects' component={Projects}/>
+    <Route path='/resume' component={Resume} />
+    <Route path='/' component={MainPage} />
+    <Route component={MainPage} />
+    </Switch>
+   </MainDiv>
   );
 }
 
---
+++ src/components/MainPage.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+
+
+
+
+export default function MainPage(props) {
+
+    
+    return (
+        <div className='main'>
+            <div className='name'>
+                <h1>Amber Mohler</h1>
+                <p>Full Stack Web Developer (In training)</p>
+            </div>
+            <div className='quote'>
+                <p>If at first you don't succeed,</p>
+                <p>call it version 1.0</p>
+            </div>
+            <a className='star-button-1' href='/projects' > </a>
+            <a className='star-button-3' href='/resume' > </a>
+            <div className='star-button-4' />
+            <p>Photo by Taylor Leopold on Unsplash</p>
+        </div>
+    )
+}
+//<a className='star-button-2' />
