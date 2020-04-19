f0ffec06f5053746575773acb7474ca56cfb0bb5
Amber Mohler <ambermohler@Ambers-iMac.local>
2020-04-19 14:38:14 -0400

    added resume presume page and background img

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,7 +3,9 @@ import React from 'react'
 export default function Projects() {
     return (
         <div className='projects'>
+            <div>
             <h2>Past Projects</h2>
+            </div>
             <div className='project'>
                 <iframe src='https://reducer-todo-teal.now.sh/' title='reducer-todo'></iframe>
             </div>
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -2,8 +2,16 @@ import React from 'react'
 
 export default function Resume() {
     return (
-        <div>
-            Hey
-        </div>
+        <>
+            <div className='resume'>
+                <div className='resume-title'>
+                    <h1>Resume and Accomplishments</h1>
+                </div>
+                </div>
+            <div className='resume-footer'>
+                <p>Photo by Casey Horner on Unsplash</p>
+            </div>
+            
+        </>
     )
 }
